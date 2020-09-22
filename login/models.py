721d828a183cb3ea143cecfa10e4be16de4c40a1
MagnaPax <chunhee.han@outlook.com>
2020-09-22 16:14:00 +0900

    Connect Oracle DB
    로그인 정보를 얻기 위해 실제 오라클 DB에 연결한 뒤 웹에서 입력된 로그인 정보와 비교 후 중복 처리

--- login/models.py
+++ login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import cx_Oracle as ora
 
-# Create your models here.
+database = 'kosmorpa/test00@192.168.0.68:1521/orcl'
+
+def getLoginChk(**kwargs):
+    con = ora.connect(database)
+    cursor = con.cursor()
+    # name, id 값을 select
+    sql_select="select count(*) cnt,name from member_table where id=:id and pwd=:pwd group by name"
+    cursor.execute(sql_select, id=kwargs['id'], pwd=kwargs['pwd'])
+    datas = cursor.fetchall()
+    con.close()
+    return datas
+옴
--- login/views.py
+++ login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 
+from login.models import getLoginChk
+
 
 def main(request):
     return render(request, 'login/index.html')
@@ -17,25 +19,36 @@ def loginform(request):
         print('user_id:', user_id)
         print('user_pwd:', user_pwd)
 
-        db_id = 'xman'
-        db_pwd = '11'
-        db_name = '김길동'
+        # db_id = 'xman'
+        # db_pwd = '11'
+        # db_name = '김길동'
+        #
+        # if user_id in db_id and user_pwd in db_pwd:
+        #     print("login complete!")
+        #     request.session['user_id'] = db_id
+        #     request.session['user_name'] = db_name
+        #     return redirect("/login")
+        # else:
+        #     print("login 실패")
+        #     msg = '아이디나 비밀번호가 잘못 되었습니다'
+        #     return render(request, "login/login.html", {"error": msg})
 
-        if user_id in db_id and user_pwd in db_pwd:
-            print("login complete!")
-            request.session['user_id'] = db_id
-            request.session['user_name'] = db_name
-            return redirect("/login")
+        res = getLoginChk(id=user_id, pwd=user_pwd)
+        print('=' * 30)
+        print(res)
+        if len(res) > 0:
+            print('로그인 성공')
+            request.session['user_id'] = user_id
+            request.session['user_name'] = res[0][1]
         else:
-            print("login 실패")
+            print('로그인 실패')
             msg = '아이디나 비밀번호가 잘못 되었습니다'
             return render(request, "login/login.html", {"error": msg})
-
         return redirect("/login")
     return render(request, "login/login.html")
 
 
-#logout
+# logout
 def logout(request):
     del request.session['user_id']
     del request.session['user_name']
